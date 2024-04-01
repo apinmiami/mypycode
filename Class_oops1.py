2ae59ed181b802d8e1b72dfb681ed18fc9ec0ef8
apinmiami <apincybers@gmail.com>
2024-04-01 14:26:35 -0400

    2nd comit of python script oops concept

---
+++ Class_oops1.py
@@ -0,0 +1,15 @@
+# THIS is example of CLASS and Object creation
+# By Anant
+class Test:
+    def display(self): # Instance method
+        print("Hi") 
+
+    def show(self):
+        print("Kem cho")
+
+# Programmer
+# Calling
+        
+T=Test()  # Creation of an Object
+T.display()  # T is instance here known as object, objects are called instance of a class
+T.show()     # . dot is instance of operator
